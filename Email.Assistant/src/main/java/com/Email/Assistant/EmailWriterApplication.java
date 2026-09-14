fd8b020e9bbcf5db5ca48a99dbbfab0511f2aa4d
abhishektiwari-dsa <heyabhishek20@gmail.com>
2026-09-14 15:02:05 +0530

    Initial commit - AI Email Reply Assistant

---
+++ Email.Assistant/src/main/java/com/Email/Assistant/EmailWriterApplication.java
@@ -0,0 +1,13 @@
+package com.Email.Assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterApplication.class, args);
+	}
+
+}
